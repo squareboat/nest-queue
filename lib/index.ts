0a4000c1c99f58e504842d21d817d4e1c152617e
Vinayak Sarawagi <vinayaksarawagi25@gmail.com>
2020-12-07 06:29:05 +0530

    CHANGE file and class names

--- lib/index.ts
+++ lib/index.ts
@@ -3,5 +3,5 @@ export * from "./interfaces";
 export * from "./service";
 export * from "./queue";
 export * from "./decorators";
+export * from "./jobrunner";
 export * from "./worker";
-export * from "./runner";
